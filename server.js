e75ce63207b1fd71615ec7bc4a041cf4ad26b38d
Khanh Tran <khanhtc@icd-vn.com>
2024-08-24 11:07:09 +0700

    change port

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ io.on('connection', (socket) => {
   console.log('a user connected');
 });
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+server.listen(3001, () => {
+  console.log('server running at http://localhost:3001');
 });
